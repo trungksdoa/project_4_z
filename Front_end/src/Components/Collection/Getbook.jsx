f2d0d12fa09dfdd1ff7b8762f4e7e0621a87c544
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-02-19 20:06:41 +0700

    Customers

--- Front_end/src/Components/Collection/Getbook.jsx
+++ Front_end/src/Components/Collection/Getbook.jsx
@@ -3,8 +3,9 @@ import PropType from 'prop-types';
 import { Link } from "react-router-dom";
 
 import { useState } from 'react';
-import { CartProvider, useCart } from "react-use-cart";
+
 import { useCookies } from 'react-cookie';
+
 Getbook.PropType = {
 	bookList: PropType.array,
 	wishlists: PropType.array,
@@ -18,6 +19,7 @@ Getbook.defaultProps = {
 	onAdd: null
 };
 
+
 function Getbook({ bookList, onAdd, addWishlist }) {
 	const [cookies, setCookie, removeCookie] = useCookies(['loggin']);
 	const auth = cookies.loggin !== undefined ? cookies.loggin.loggin : false;
@@ -26,92 +28,90 @@ function Getbook({ bookList, onAdd, addWishlist }) {
 			addWishlist(bookId)
 		}
 	}
-	const { addItem } = useCart();
-	function onClick(props) {
-		const { booksid, bookprice, pdetailid, bookname } = props;
-		const newObject = { id: "", price: 0, img: "", name: "" };
-		newObject.id = booksid;
-		newObject.price = bookprice;
-		newObject.img = pdetailid.imageLink;
-		newObject.name = bookname;
-		addItem(newObject);
-		console.log(newObject);
+	const handleAddToCart = (props) => {
+		if (onAdd) {
+			onAdd(props)
+		}
 	}
 	return (
-		<div className="tg-productgrid">
-
-			{bookList.map((post, index) => (
-				<div key={index} className="col-xs-6 col-sm-6 col-md-4 col-lg-3">
-					<div className="tg-postbook">
-						<figure className="tg-featureimg">
-							<div className="tg-bookimg">
-								<div className="tg-frontcover">
-									<img key={post.booksid} src={"http://localhost:9999/image/" + post.pdetailid.imageLink + "?v=" + new Date().getTime()} alt="image description" />
-								</div>
-								<div className="tg-backcover">
-									<img key={post.booksid} src={"http://localhost:9999/image/" + post.pdetailid.imageLink + "?v=" + new Date().getTime()} alt="image description" />
-								</div>
-							</div>
-							{auth && (
-								post.wishlists.length !== 0 &&
-								(
-									post.wishlists.map((wishCheck, index) => {
-										return (
-											<a key={index} className="tg-btnaddtowishlist" style={{ backgroundColor: 'green' }}>
-												<span>Already in wishlist</span>
+		<>
+			<div className="tg-productgrid">
+				{bookList.map((post, index) => {
+					console.log(post)
+					return (
+						<>
+							<div key={index} className="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+								<div className="tg-postbook">
+									<figure className="tg-featureimg">
+										<div className="tg-bookimg">
+											<div className="tg-frontcover">
+												<img key={post.booksid} src={"http://localhost:9999/image/" + post.pdetailid.imageLink + "?v=" + new Date().getTime()} alt="image description" />
+											</div>
+											<div className="tg-backcover">
+												<img key={post.booksid} src={"http://localhost:9999/image/" + post.pdetailid.imageLink + "?v=" + new Date().getTime()} alt="image description" />
+											</div>
+										</div>
+										{auth && (
+											post.wishlists.length !== 0 &&
+											(
+												post.wishlists.map((wishCheck, index) => {
+													return (
+														<a key={index} className="tg-btnaddtowishlist" style={{ backgroundColor: 'green' }}>
+															<span>Already in wishlist</span>
+														</a>
+													)
+												})
+											)
+										)}
+										{auth && (
+											post.wishlists.length === 0 &&
+											(
+												<a className="tg-btnaddtowishlist" onClick={() => handleWishList(post.booksid)} style={{ cursor: 'pointer' }}>
+													<i className="icon-heart" />
+													<span>add to wishlist</span>
+												</a>
+											)
+										)}
+										{!auth && (
+											<a className="tg-btnaddtowishlist" style={{ cursor: 'pointer' }}>
+												<span>Please login to use</span>
 											</a>
-										)
-									})
-								)
-							)}
-							{auth && (
-								post.wishlists.length === 0 &&
-								(
-									<a className="tg-btnaddtowishlist" onClick={() => handleWishList(post.booksid)} style={{ cursor: 'pointer' }}>
-										<i className="icon-heart" />
-										<span>add to wishlist</span>
-									</a>
-								)
-							)}
-							{!auth && (
-								<a className="tg-btnaddtowishlist" style={{ cursor: 'pointer' }}>
-									<span>Please login to use</span>
-								</a>
-							)}
-						</figure>
-						<div className="tg-postbookcontent">
-							<ul className="tg-bookscategories">
-								<li>
-									<a href="javascript:void(0);">Language:{post.pdetailid.language}</a>
-								</li>
-							</ul>
-							<div className="tg-themetagbox">
-								<span className="tg-themetag">sale</span>
+										)}
+									</figure>
+									<div className="tg-postbookcontent">
+										<ul className="tg-bookscategories">
+											<li>
+												<a href="javascript:void(0);">Language:{post.pdetailid.language}</a>
+											</li>
+										</ul>
+										<div className="tg-themetagbox">
+											<span className="tg-themetag">sale</span>
+										</div>
+										<div className="tg-booktitle">
+											<Link to={"/Book/" + post.booksid}>{post.bookname}</Link>
+										</div>
+										<span className="tg-bookwriter">
+											By: <a style={{ cursor: 'pointer' }}  key={post.booksid}>{post.authorid.authorname}</a>
+										</span>
+										<span className="tg-stars">
+											<span />
+										</span>
+										<span className="tg-bookprice">
+											<ins key={post.booksid} >${post.bookprice}</ins>
+
+										</span>
+										<a className="tg-btn tg-btnstyletwo" style={{ cursor: 'pointer' }}>
+											<i className="fa fa-shopping-basket" />
+											<em onClick={() => handleAddToCart(post)}>Add To Basket</em>
+										</a>
+									</div>
+								</div>
 							</div>
-							<div className="tg-booktitle">
-								<Link to={"/Book/" + post.booksid}>{post.bookname}</Link>
-							</div>
-							<span className="tg-bookwriter">
-								By: <a href="javascript:void(0);" key={post.booksid}>{post.authorid.authorname}</a>
-							</span>
-							<span className="tg-stars">
-								<span />
-							</span>
-							<span className="tg-bookprice">
-								<ins key={post.booksid} >${post.bookprice}</ins>
-
-							</span>
-							<a className="tg-btn tg-btnstyletwo" href="javascript:void(0);">
-								<i className="fa fa-shopping-basket" />
-								<em onClick={() => onClick(post)}>Add To Basket</em>
-							</a>
-						</div>
-					</div>
-				</div>
-			))}
-
-		</div>
-
+						</>
+					)
+				})}
+			</div>
+		</>
 	);
 }
 
--- Front_end/src/Components/Header/Basket.jsx
+++ Front_end/src/Components/Header/Basket.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { CartProvider, useCart } from 'react-use-cart';
 import { useState } from 'react';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+import './App.css'
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	...theme.typography.body2,
+	padding: "1rem",
+	width: "35px",
+	cursor: 'pointer',
+	position: "relative",
+	textAlign: 'center',
+	color: theme.palette.text.secondary,
+}));
 
 export default function Basket(props) {
+	const { open } = props;
 	const {
 		cartTotal,
 		isEmpty,
@@ -15,30 +32,18 @@ export default function Basket(props) {
 	//   const { cartItems, onAdd, onRemove } = props;
 	//   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
 	//   const totalPrice = itemsPrice;
-	const [ data, setData ] = useState('');
-	console.log(items);
 	return (
-		<div className="dropdown tg-themedropdown tg-minicartdropdown">
-			<a
-				href="#!"
-				id="tg-minicart"
-				className="tg-btnthemedropdown"
-				data-toggle="dropdown"
-				aria-haspopup="true"
-				aria-expanded="false"
-			>
-				<span className="tg-themebadge">({totalUniqueItems})</span>
-				<i className="icon-cart" />
-				<span />
-			</a>
-
-			<div className="dropdown-menu tg-themedropdownmenu" aria-labelledby="tg-minicart">
-				<div className="tg-minicartbody">
-					{items.map((item, index) => {
-						if (index <= 2) {
-							return (
+		<div className="dropdown-menu tg-themedropdownmenu" aria-labelledby="tg-minicart">
+			{items.length === 0 && (
+				<div className="tg-description"><p>No products were added to the cart!</p></div>
+			)}
+			<div className="tg-minicartbody">
+				{items.length !== 0 && items.map((item, index) => {
+					if (index <= 2) {
+						return (
+							<>
 								<div className="tg-minicarproduct">
-									<figure>
+									<figure className="image_with_badge_container">
 										<img
 											width={70}
 											height={70}
@@ -47,31 +52,41 @@ export default function Basket(props) {
 											}
 											alt="image description"
 										/>
+										<span class="badge badge-on-image">{item.quantity}</span>
 									</figure>
 									<div className="tg-minicarproductdata">
 										<h5>
 											<a href="#!">{item.name}</a>
 										</h5>
-										<h6>
-											<a href="#!">
-												$ {item.quantity} x ${item.price.toFixed(2)}
-											</a>
-										</h6>{' '}
-										<h3>
-											<button onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>
-												-
-											</button>
-											<button onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>
-												+
-											</button>
-											<button onClick={() => removeItem(item.id)}>&times;</button>
-										</h3>
+										<p>
+											${item.price.toFixed(2)}
+											<hr />
+											<Stack direction="row" spacing={2}>
+												<Item onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>
+													<RemoveIcon style={{
+														position: "absolute",
+														/* text-align: center; */
+														/* top: 50%; */
+														top: "8px",
+														left: "11px",
+													}}></RemoveIcon>
+												</Item>
+												<Item onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>
+													<i className="fa fa-plus fa-lg"></i>
+												</Item>
+												<Item onClick={() => removeItem(item.id)}>
+													<i class="fa fa-times fa-lg" aria-hidden="true" ></i>
+												</Item>
+											</Stack>
+										</p>
 									</div>
 								</div>
-							);
-						}
-					})}
-				</div>
+							</>
+						);
+					}
+				})}
+			</div>
+			{items.length !== 0 && (
 				<div className="tg-minicartfoot">
 					<span className="tg-subtotal">
 						Subtotal: <strong>${cartTotal}</strong>
@@ -85,7 +100,7 @@ export default function Basket(props) {
 						</a>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
